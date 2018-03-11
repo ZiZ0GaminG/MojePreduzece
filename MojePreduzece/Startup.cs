c3944ee3401b21347bd5f7db71d3b879ed731432
nemanja1997 <37266494+nemanja1997@users.noreply.github.com>
2018-03-11 14:43:53 +0100

    Add project files.

---
+++ MojePreduzece/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MojePreduzece.Startup))]
+namespace MojePreduzece
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
